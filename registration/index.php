3c05bcac350998965b4e8154a5710b69267bd12a
root <root@studvm27-p.cs.ucl.ac.uk>
2014-10-28 15:41:19 +0000

    test

--- registration/index.php
+++ registration/index.php
@@ -19,7 +19,7 @@
 <h1>Register here!</h1>
 <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
 <form method="post" action="index.php" enctype="multipart/form-data" >
-      Name  <input type="text" name="name" id="name"/></br>
+      sdfsd <input type="text" name="name" id="name"/></br>
       Company <input type="text" name="company" id="company"/></br>
       Email <input type="text" name="email" id="email"/></br>
       <input type="submit" name="submit" value="Submit" />
